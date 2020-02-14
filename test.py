9078624602b103476157593a09cbcd27ef9f2147
ventolaz4 <38504089+ventolaz4@users.noreply.github.com>
2020-02-14 13:04:20 -0500

    Version 1

---
+++ test.py
@@ -0,0 +1,42 @@
+'''
+Created on Feb 6, 2020
+
+@author: Normal
+'''
+import subprocess
+import os
+
+# Check to make sure file exists. If not, keep asking until valid path is given.
+file_exists = True
+while file_exists:     
+    # ask user for path name of makefile and save into a variable
+    print('Enter the path name for your makefile:')
+    makefile_input = input()
+    # Check path exists
+    if os.path.exists(makefile_input):
+        print('File located, beginning scan')
+        file_exists = False
+    else:
+        print('Invalid file location. Try again.')
+        file_exists = True
+
+# Find all lines that have include paths and store them in an array
+makefile_path = open(makefile_input)
+include_paths = []
+for line in makefile_path:
+    if '-I' in line: 
+        include_paths.append(line)
+
+# Loop to remove all white space characters before and after string
+counter = 0
+IncludePathsCounter = len(include_paths)
+while counter < IncludePathsCounter:
+    tempString = include_paths[counter]
+    include_paths[counter] = " ".join(tempString.split())
+    counter += 1
+
+# Print out strings
+for i in include_paths:
+    print(i)
+
+
